166652058f2f0e2b7055e9219e7d7665fbf13d95
Michael Pascucci <mdpascucci@live.com>
2022-05-18 16:27:27 -0400

    Initial Commit

---
+++ Instafilter/InstafilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstafilterApp.swift
+//  Instafilter
+//
+//  Created by Michael & Diana Pascucci on 5/18/22.
+//
+
+import SwiftUI
+
+@main
+struct InstafilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
