70ae18d6e8977f70b91ed2475060cd810b6db0ac
Michael Pascucci <mdpascucci@live.com>
2022-05-19 16:09:49 -0400

    Complete project with challenges

--- Instafilter/ContentView.swift
+++ Instafilter/ContentView.swift
@@ -14,6 +14,9 @@ struct ContentView: View {
     // MARK: - PROPERTIES
     @State private var image: Image?
     @State private var filterIntensity = 0.5
+    @State private var filterRadius = 200.0
+    @State private var filterScale = 10.0
+    @State private var filterAngle = 3.1
     
     @State private var showingImagePicker = false
     @State private var inputImage: UIImage?
@@ -24,6 +27,8 @@ struct ContentView: View {
     @State private var showingFilterSheet = false
     
     @State private var processedImage: UIImage?
+    
+    @State private var noPictureSelected = true
     
     // MARK: - BODY
     var body: some View {
@@ -45,12 +50,38 @@ struct ContentView: View {
                     showingImagePicker = true
                 }
                 
-                HStack {
-                    Text("Intensity")
-                    Slider(value: $filterIntensity)
-                        .onChange(of: filterIntensity) { _ in
-                            applyProcessing()
-                        }
+                VStack {
+                    HStack {
+                        Text("Intensity (\(filterIntensity, specifier: "%.1f"))")
+                        Slider(value: $filterIntensity, in: 0...1, step: 0.1)
+                            .onChange(of: filterIntensity) { _ in
+                                applyProcessing()
+                            }
+                    }
+                    
+                    HStack {
+                        Text("Radius (\(filterRadius, specifier: "%.0f"))")
+                        Slider(value: $filterRadius, in: 0...1000, step: 1)
+                            .onChange(of: filterRadius) { _ in
+                                applyProcessing()
+                            }
+                    }
+                    
+                    HStack {
+                        Text("Scale (\(filterScale, specifier: "%.0f"))")
+                        Slider(value: $filterScale, in: 0...20, step: 1)
+                            .onChange(of: filterScale) { _ in
+                                applyProcessing()
+                            }
+                    }
+                    
+                    HStack {
+                        Text("Angle (\(filterAngle, specifier: "%.0f"))")
+                        Slider(value: $filterAngle, in: 0...12.5, step: 0.5)
+                            .onChange(of: filterAngle) { _ in
+                                applyProcessing()
+                            }
+                    }
                 }
                 .padding(.vertical)
                 
@@ -62,6 +93,7 @@ struct ContentView: View {
                     Spacer()
                     
                     Button("Save", action: save)
+                        .disabled(noPictureSelected)
                 }
             }
             .padding([.horizontal, .bottom])
@@ -70,13 +102,14 @@ struct ContentView: View {
                 ImagePicker(image: $inputImage)
             }
             .confirmationDialog("Select a filter", isPresented: $showingFilterSheet) {
-                Button("Crystallize") { setFilter(CIFilter.crystallize()) }
-                Button("Edges") { setFilter(CIFilter.edges()) }
-                Button("Gaussian Blur") { setFilter(CIFilter.gaussianBlur()) }
-                Button("Pixellate") { setFilter(CIFilter.pixellate()) }
-                Button("Sepia Tone") { setFilter(CIFilter.sepiaTone()) }
-                Button("Unsharp Mask") { setFilter(CIFilter.unsharpMask()) }
-                Button("Vignette") { setFilter(CIFilter.vignette()) }
+                Button("Crystallize") { setFilter(CIFilter.crystallize()) }         // Requires Radius (Default: 20.0)
+                Button("Edges") { setFilter(CIFilter.edges()) }                     // Requires Intensity (Default 1.0)
+                Button("Gaussian Blur") { setFilter(CIFilter.gaussianBlur()) }      // Requires Radius (Default: 10.0)
+                Button("Pixellate") { setFilter(CIFilter.pixellate()) }             // Requires Scale (Default: 8.0)
+                Button("Sepia Tone") { setFilter(CIFilter.sepiaTone()) }            // Requires Intensity (Default: 1.0)
+                Button("Unsharp Mask") { setFilter(CIFilter.unsharpMask()) }        // Requires Intensity (Default: 0.5) and Radius (Default 2.5)
+                Button("Vignette") { setFilter(CIFilter.vignette()) }               // Requires Intensity (Default: 0.0) and Radius (Default: 1.0)
+                Button("Twirl Distortion") { setFilter(CIFilter.twirlDistortion()) } // Requires Radius (Default: 300.0) and Angle (Defualt: 3.14)
                 Button("Cancel", role: .cancel) { }
             }
             .onChange(of: inputImage) { _ in loadImage() }
@@ -94,8 +127,9 @@ struct ContentView: View {
     func applyProcessing() {
         let inputKeys = currentFilter.inputKeys
         if inputKeys.contains(kCIInputIntensityKey) { currentFilter.setValue(filterIntensity, forKey: kCIInputIntensityKey) }
-        if inputKeys.contains(kCIInputRadiusKey) { currentFilter.setValue(filterIntensity * 200, forKey: kCIInputRadiusKey) }
-        if inputKeys.contains(kCIInputScaleKey) { currentFilter.setValue(filterIntensity * 10, forKey: kCIInputScaleKey) }
+        if inputKeys.contains(kCIInputRadiusKey) { currentFilter.setValue(filterIntensity * filterRadius, forKey: kCIInputRadiusKey) }
+        if inputKeys.contains(kCIInputScaleKey) { currentFilter.setValue(filterIntensity * filterScale, forKey: kCIInputScaleKey) }
+        if inputKeys.contains(kCIInputAngleKey) { currentFilter.setValue(filterAngle, forKey: kCIInputAngleKey)}
         
         guard let outputImage = currentFilter.outputImage else { return }
         
@@ -103,6 +137,8 @@ struct ContentView: View {
             let uiImage = UIImage(cgImage: cgimg)
             image = Image(uiImage: uiImage)
             processedImage = uiImage
+            
+            noPictureSelected = false
         }
     }
     
